728b23c66bea96a587d0b65951227bd66f5adb3b
KAYESU RITAH <rkayesu464@gmail.com>
2022-04-20 18:02:53 +0300

    Add files via upload

---
+++ ARMSweep.ino
@@ -0,0 +1,46 @@
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ http://www.arduino.cc/en/Tutorial/Sweep
+*/
+
+#include <Servo.h>
+
+Servo base;  // create servo object to control a servo
+Servo elbow;
+Servo shoulder;
+Servo grip;
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  base.attach(9);  // attaches the servo on pin 9 to the servo object
+  shoulder.attach(3);
+  elbow.attach(5);
+  grip.attach(6);
+}
+
+void loop() {
+  for (pos = 0; pos <= 90; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    base.write(pos);              // tell servo to go to position in variable 'pos'
+    elbow.write(pos); 
+    delay(5);                       // waits 15ms for the servo to reach the position
+    shoulder.write(pos); 
+   grip.write(pos); 
+    delay(5);
+  }
+  for (pos = 90; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+       base.write(pos);              // tell servo to go to position in variable 'pos'
+      elbow.write(pos); 
+      delay(5);                       // waits 15ms for the servo to reach the position
+      shoulder.write(pos); 
+      grip.write(pos); 
+     delay(5);                       // waits 15ms for the servo to reach the position
+  }
+}
+
---
+++ armONLY.ino
@@ -0,0 +1,61 @@
+// Simple Bluetooth Controlled robot arm using kodular
+// by AXISDYNAMICS (Kayesu Ritah)
+// Arduino UNO
+// HC-040 Bluetooth module
+// motor+relay circuit, 6 micro servo motors
+
+#include <SoftwareSerial.h> //TX,RX software library
+#include<Servo.h>// include server library
+
+Servo base, shoulder, elbow, grip; // create servo objects to control a servos at neck, base, shoulder, elbow, wrist and grip
+char m;
+
+SoftwareSerial bluetooth(0,1); //defining a new serial port called bluetooth(Tx,Rx)
+
+void setup(){
+Serial.begin(9600); // Serial comm begin at 9600bps
+bluetooth.begin(9600); //Setup Bluetooth serial connection to download
+base.attach(6);// servo for base is connected at pin 6
+shoulder.attach(5);// servo for shoulder is connected at pin 5
+elbow.attach(3);//servo for elbow is connected at pin 3
+grip.attach(9);// servo for grip is connected at pin 9
+}
+
+void loop(){
+if(bluetooth.available() && bluetooth.available()>= 2 ){
+    unsigned int servopos = bluetooth.read();
+    unsigned int servopos1 = bluetooth.read();
+    unsigned int realservo = (servopos1 *256) + servopos;
+    Serial.println(realservo);
+
+    if (realservo >= 1000 && realservo <1180) {
+      int servo1 = realservo;
+      servo1 = map(servo1, 1000, 1180, 0, 180);
+      base.write(servo1);
+      Serial.println("Turning base");
+      delay(5);
+    }
+    if (realservo >= 2000 && realservo <2090) {
+      int servo2 = realservo;
+      servo2 = map(servo2, 2000, 2090, 0, 90);
+      shoulder.write(servo2);
+      Serial.println("Moving shoulder");
+      delay(5);
+    }
+    if (realservo >= 3000 && realservo <3090) {
+      int servo3 = realservo;
+      servo3 = map(servo3, 3000, 3090, 0, 90);
+      elbow.write(servo3);
+      Serial.println("Moving elbow");
+      delay(5);
+    }
+    if (realservo >= 4000 && realservo <4090) {
+      int servo4 = realservo;
+      servo4 = map(servo4, 4000, 4090, 0, 90);
+      grip.write(servo4);
+      Serial.println("Gripping");
+      delay(5);
+    }
+  }
+}
+
